7692697e2057cb7c81ae05156342266ce68d995a
yoery <81920433+yoery@users.noreply.github.com>
2022-11-15 11:20:48 +0100

    Add files via upload

---
+++ h2.php
@@ -0,0 +1,48 @@
+<?php
+//$image_url = "https://pcbomen.nl/wp-content/uploads/bfi_thumb/PCBomen-bomen-zomer-blog-p8ct6z9l1kpnakrz6btrbdme8vqs1dgpton9dwoxw4.jpg";
+
+//for($image = 0; $image <= 9; $image++){
+ //   echo "<img src='$image_url'>";
+//}
+
+    //for($i = 0; $i <= 9; $i++){
+    // for($j = 0; $j<=$i; $j++){
+    //    echo"*";
+   //  }
+   // echo"*<br>";
+//}
+
+//$x = 35;
+//for($x = 0; $x >= 25; $x++) {
+//echo "hoppelepee";
+//$x--;
+//}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+       <style>
+            body{
+               text-align: center;
+        }
+
+            .rood{
+                border: red solid 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        //for($i = 1; $i <= 9; $i++){
+        //    if($i == 3){
+        //        $class = "class= 'rood'";
+        //    }else{
+        //        $class = "";
+        //    }
+        //    echo "<img ".$class. "src='apen/aap".$i.".jpg'>";
+      //  }
+
+        ?>
+    </body>
+</html
---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+$d = "die gaan fout";
+$e = "en";
+$f = "niet";
+$g = "ry";
+$h = ", but";
+$i = "fail";
+$j = "\"omgaan";
+$k = "met";
+$l = "telue";
+
+
+echo $a .$b .$c; 
+echo $d .$e .$f;
+echo $g .$h .$i;
+echo $j .$k .$l;
+?>
